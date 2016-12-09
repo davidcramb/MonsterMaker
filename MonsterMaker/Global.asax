8c290acab3b74812ecb9fbade75ccdfb2c74fefe
David Cramb <david.cramb@gmail.com>
2016-12-08 21:26:12 -0600

    initial commit, working project directory.

---
+++ MonsterMaker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MonsterMaker.MvcApplication" Language="C#" %>
---
+++ MonsterMaker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MonsterMaker.Startup))]
+namespace MonsterMaker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
