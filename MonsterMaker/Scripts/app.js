4322e18755b733bf297a1acdfb45a03c4560b8a5
David Cramb <david.cramb@gmail.com>
2016-12-25 08:46:47 -0600

    Added canvas popins for body types. Must refactor simple coordinates next

--- MonsterMaker/Scripts/app.js
+++ MonsterMaker/Scripts/app.js
@@ -41,8 +41,32 @@ app.controller("MonsterCreateCtrl", function ($scope, $http) {
         image.src = bodyimage;
         ctx.drawImage(image, 50, 50);
     }
+    $scope.DrawHead = (headimage) => {
+        var image = new Image();
+        image.src = headimage;
+        ctx.drawImage(image, 50, 20);
+    }
+    $scope.DrawArms = (armimage) => {
+        var image = new Image();
+        image.src = armimage;
+        ctx.drawImage(image, 10, 50);
+        ctx.drawImage(image, 30, 50);
+    }
+    $scope.DrawLegs = (legimage) => {
+        var image = new Image();
+        image.src = legimage;
+        cx.drawImage(image, 10, 80);
+        ctx.drawImage(image, 30, 80);
+    }
+    $scope.DrawExtra = (extraimage) => {
+        var image = new Image();
+        image.src = extraimage;
+        ctx.drawImage(image);
+    }
     $scope.DrawHeadType = (id) => {
         $http.get("api/MonsterDetail/Head/" + id).success(function (response) {
+            var headImage = response.ImageURL;
+            $scope.DrawHead(headImage);
             console.log(response);
         }).error(function (error) {
             console.log(error);
@@ -50,6 +74,8 @@ app.controller("MonsterCreateCtrl", function ($scope, $http) {
     }
     $scope.DrawArmType = (id) => {
         $http.get("api/MonsterDetail/Arm/" + id).success(function (response) {
+            var armImage = response.ImageURL;
+            $scope.DrawArms(armImage);
             console.log(response);
         }).error(function (error) {
             console.log(error);
@@ -57,6 +83,8 @@ app.controller("MonsterCreateCtrl", function ($scope, $http) {
     }
     $scope.DrawLegType = (id) => {
         $http.get("api/MonsterDetail/Leg/" + id).success(function (response) {
+            var legImage = response.ImageURL;
+            $scope.DrawLegs(legImage);
             console.log(response);
         }).error(function (error) {
             console.log(error)
@@ -64,6 +92,8 @@ app.controller("MonsterCreateCtrl", function ($scope, $http) {
     }
     $scope.DrawAccessoryType = (id) => {
         $http.get("api/MonsterDetail/Accessory/" + id).success(function (response) {
+            var extraImage = response.ImageURL;
+            $scope.DrawExtra(extraImage);
             console.log(response);
         }).error(function (error) {
             console.log(error);
